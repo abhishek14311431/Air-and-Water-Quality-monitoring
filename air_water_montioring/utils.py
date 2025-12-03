465ca6031c34c80f165e320b7ab3b851d58db572
abhishek yadav <152249166+abhishek14311431@users.noreply.github.com>
2025-12-04 00:34:00 +0530

    Update utils.py

--- air_water_montioring/utils.py
+++ air_water_montioring/utils.py
@@ -1,18 +1,14 @@
 import requests
-import os
-
-API_KEY = os.getenv("AQI_API_KEY")
+import streamlit as st
 
 def get_air_quality_for_city(city_name: str):
+    API_KEY = st.secrets.get("openweather_api_key")
+
     if not API_KEY:
-        raise Exception("API Key not found. Please add AQI_API_KEY in Streamlit Secrets.")
+        raise Exception("API Key not found. Add `openweather_api_key` in secrets.toml")
 
     geo_url = "https://api.openweathermap.org/geo/1.0/direct"
-    geo_params = {
-        "q": city_name,
-        "limit": 1,
-        "appid": API_KEY
-    }
+    geo_params = {"q": city_name, "limit": 1, "appid": API_KEY}
 
     geo_resp = requests.get(geo_url, params=geo_params)
     geo_data = geo_resp.json()
@@ -24,11 +20,7 @@ def get_air_quality_for_city(city_name: str):
     lon = geo_data[0]["lon"]
 
     air_url = "https://api.openweathermap.org/data/2.5/air_pollution"
-    air_params = {
-        "lat": lat,
-        "lon": lon,
-        "appid": API_KEY
-    }
+    air_params = {"lat": lat, "lon": lon, "appid": API_KEY}
 
     air_resp = requests.get(air_url, params=air_params)
     air_data = air_resp.json()
@@ -43,4 +35,3 @@ def get_air_quality_for_city(city_name: str):
         "o3": comp["o3"],
         "co": comp["co"],
     }
-
